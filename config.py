ba9a4ab42a020163b360aa839ae501da4eaa75a8
vegabryan22 <vegabryan@gmail.com>
2026-06-03 09:16:55 -0600

    Usar usuario de base local como configuracion por defecto

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "expotecnica-secret-key")
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
-        "mysql+pymysql://root:123456@localhost/expotecnica_db?charset=utf8mb4",
+        "mysql+pymysql://expotecnica_user:expotecnica123@localhost/expotecnica_db?charset=utf8mb4",
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
